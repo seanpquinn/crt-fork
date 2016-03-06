90d36b424fe720d621ffdbc94619c1f634aaf1da
Sean Quinn <spq@case.edu>
2016-03-06 11:54:18 -0500

    Housekeeping, added some doc

--- MC.py
+++ MC.py
@@ -6,22 +6,15 @@ import datetime as dt
 import os
 import sys
 
-n=70
+n=2*10**5 #num samples
 
-#l=348.63
-#lat=-21.11
-#E=113.06
-#eid=152364323000
-
-a1,b1=(13.8-18.)/2.5,(20.-18.)/2.5
-a2,b2=(0.-0.2)/0.12,(np.inf-0.2)/0.12
+#Truncated Gaussian interval params
+a1,b1=(12.6-18.25)/2.75,(19.5-18.25)/2.75
+a2,b2=(0.-0.2)/0.12,(1-0.2)/0.12
 
 def backtrack(counter):
-  #First determine how many params to vary
-  #num_params=np.random.randint(1,20,size=1)[0]
-  #Draw num_params from param list without replacement
-  #params_used=np.random.choice(np.arange(1,21),num_params,replace=False)
-  np.random.seed(counter+rndseed)
+  """Performs pRNG, calls CRT, then saves results in memory"""
+  np.random.seed(counter+rndseed) #Start seed is randomly assigned
   params=np.array([np.random.normal(0.1,1.8),
     np.random.normal(3.0,0.6),
     np.random.normal(-0.9,0.8),
@@ -35,7 +28,7 @@ def backtrack(counter):
     np.random.normal(1.4,0.1),
     np.random.normal(-1.1,0.1),
     np.random.normal(9.22,0.08),
-    ss.truncnorm.rvs(a1,b1,18.,2.5,size=1),
+    ss.truncnorm.rvs(a1,b1,18.25,2.75,size=1),
     ss.truncnorm.rvs(a2,b2,0.2,0.12,size=1),
     np.random.normal(5.3,1.6),
     np.random.normal(4.6,0.3),
@@ -43,25 +36,35 @@ def backtrack(counter):
     np.random.normal(4.8,0.2),
     np.random.normal(2.9,0.1)
     ])
+  #Text data that is usually read from a file is instead
+  #converted to a byte array and piped directly to stdin
   line1=bytearray('C %.2f %.2f %.2f 1 -1\n' %(E,lon,lat),'ascii')
   line2=bytearray('F jf2012 0 0'+''.join([' %.6f']*len(params)) % tuple(params),'ascii')
-  #proc=sp.Popen(['./bin/CRT','-backtrack','-rawinp'],stdin=sp.PIPE,stdout=sp.PIPE,stderr=sp.PIPE,close_fds=True)
-  proc=sp.Popen(['./bin/CRT','-backtrack','-rawinp'],stdin=sp.PIPE,stdout=sp.PIPE,stderr=sp.PIPE,close_fds=True)
+  #Call CRT using timeout to avoid spurious hangs
+  proc=sp.Popen(['timeout','3','./bin/CRT','-backtrack','-rawinp'],stdin=sp.PIPE,stdout=sp.PIPE,stderr=sp.PIPE,close_fds=True)
   proc.stdin.write(line1)
   proc.stdin.write(line2)
+  #Save text output to list in memory
   out,err=proc.communicate()
   out=out.decode('ascii').split('\t')
-  #print(out)
-  return float(out[21]),float(out[22])
+  #Try except to record output long, lat, timeout flag
+  try:
+    return float(out[21]),float(out[22]),int(out[-1][0])
+  except:
+    print("Timeout condition")
+    return 0,0,1
 
+#Pre-processed Herald data file
 idarr,Earr,lonarr,latarr=np.loadtxt('data/events50_parsed.txt',
   dtype={'names':('ID','E','lon','lat'),
-  'formats':('int','float','float','float')},skiprows=7,unpack=True)
+  'formats':('int','float','float','float')},skiprows=166,unpack=True)
 
 nevents=len(idarr)
+#Record date info for run duration
 begin=dt.datetime.now()
 beginstr=dt.datetime.now().strftime("%c")
 print("Starting program: %s" %beginstr)
+#Main loop
 for j in range(nevents):
   #Globals rndseed, E, lon, and lat must be specified for each loop iteration
   rndseed=np.random.randint(0,9*10**7)
@@ -72,11 +75,13 @@ for j in range(nevents):
   start=dt.datetime.now()
   startstr=dt.datetime.now().strftime("%c")
   print("\nStart sampling for Event %i: %s" %(eid,startstr))
+  #Take advantage of multiple cores. My machine has 4
   pool = mp.Pool(processes=4)
-  srcl,srcb=zip(*pool.map(backtrack,range(n)))
-  src=np.zeros((len(srcl),2))
+  srcl,srcb,status=zip(*pool.map(backtrack,range(n)))
+  src=np.zeros((len(srcl),3))
   src[:,0]=srcl
   src[:,1]=srcb
+  src[:,2]=status
   np.save('evt_%i' %eid,src)
   pool.close()
   stop=dt.datetime.now()
